9b153db97fa02093f9f1bab6fd5b885e8f9e8259
chashikajw <chashikajw007@gmail.com>
2021-04-02 01:38:30 +0530

    add server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.setHeader('content-type', 'application/erro+json');	
   res.send('Hello World!')
 })
